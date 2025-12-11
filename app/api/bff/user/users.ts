a795d63593f38c19c704cb5372164e4b6d204489
Iris <i-wildauer@t-online.de>
2025-12-11 22:53:18 +0100

    adds users in backend

---
+++ app/api/bff/user/users.ts
@@ -0,0 +1,35 @@
+const USERS = [
+    {
+        id: 1,
+        username: "A",
+        preferences: {
+            internet: 10,
+            insurance: 20
+        }
+    },
+    {
+        id: 2,
+        username: "B",
+        preferences: {
+            internet: 20,
+            insurance: 10
+        }
+    }
+] as const;
+
+export function getUser(username: string){
+    return USERS.find(u => u.username == username);
+}
+
+export function getAllUsersNames(){
+    return USERS.map(user =>({
+        username: user.username,
+        id: user.id
+    }));
+}
+
+export function getAllUserData(){
+    return USERS;
+}
+
+//nur bff sollte direkt zugreifen, das frontend sollte eig per http die daten bekommen
